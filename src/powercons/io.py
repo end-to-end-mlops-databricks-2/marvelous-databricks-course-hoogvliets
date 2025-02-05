62841b8da30f24df8f8483e59317dab3f9de293e
Raphaël Hoogvliets <raphael.hoogvliets@gmail.com>
2025-02-05 13:25:40 +0100

    lint correction

--- src/powercons/io.py
+++ src/powercons/io.py
@@ -1,10 +1,10 @@
-from pandas import DataFrame, read_csv
-from loguru import logger
 from pathlib import Path
+
+from loguru import logger
+from pandas import DataFrame, read_csv
+
 
 def load_data(path: str) -> DataFrame:
     logger.info("Path to dataset file: %s", path)
-    df = read_csv(Path(".")/path)
+    df = read_csv(Path(".") / path)
     return df
-
-
